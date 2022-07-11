324c0257a8d22120982802ef214322261d25c538
Navya <97428614+Navya-relli@users.noreply.github.com>
2022-07-11 22:05:35 +0530

    s1=str(input()) s2=str(input()) a=s1.lower() b=s2.lower() d=list(a) e=list(b) f=[] g=[] c='abcdefghijklmnopqrstuvwxyz' for i in d:     if i!='':         if i in e:             f.append(i) for i in e:     if i!='':         if i in d:             f.append(i) for i in c:     if i in f:         g.append(i) print(len(g))

---
+++ common_characters_-II.py
@@ -0,0 +1,21 @@
+s1=str(input())
+s2=str(input())
+a=s1.lower()
+b=s2.lower()
+d=list(a)
+e=list(b)
+f=[]
+g=[]
+c='abcdefghijklmnopqrstuvwxyz'
+for i in d:
+    if i!='':
+        if i in e:
+            f.append(i)
+for i in e:
+    if i!='':
+        if i in d:
+            f.append(i)
+for i in c:
+    if i in f:
+        g.append(i)
+print(len(g))
