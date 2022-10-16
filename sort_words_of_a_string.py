062d121c3a8f76f995e47d39647211fdeef215c0
Navya <97428614+Navya-relli@users.noreply.github.com>
2022-10-16 10:44:55 +0530

    x=list(map(str,input().split())) a='~!@#$%^&*()_-?.,<>{}[]' m=[] for i in x:     for j in i:         if j not in a:             m.append(j)     m.sort()     k=0     for j in i:         if j in a:             print(j,end="")         else:             print(m[k],end="")             k+=1     print(end=" ")     m.clear()

---
+++ sort_words_of_a_string.py
@@ -0,0 +1,17 @@
+x=list(map(str,input().split()))
+a='~!@#$%^&*()_-?.,<>{}[]'
+m=[]
+for i in x:
+    for j in i:
+        if j not in a:
+            m.append(j)
+    m.sort()
+    k=0
+    for j in i:
+        if j in a:
+            print(j,end="")
+        else:
+            print(m[k],end="")
+            k+=1
+    print(end=" ")
+    m.clear()
