7da305b18e4ffa7bb0c529a557881d397b2bf61e
Navya <97428614+Navya-relli@users.noreply.github.com>
2022-10-16 12:17:02 +0530

    def anagram(s):     a=[]     for ch in s.lower():         a.append(ch)     d={}     for ch in a:         if ch not in d:             d[ch]=1         else:             d[ch]=d[ch]+1     return d s1=input() s2=input() a=anagram(s1) b=anagram(s2) if a==b:     print("True") else:     print("False")

--- Anagrams.py
+++ Anagrams.py
@@ -1,12 +1,19 @@
-n1=input()
-n2=input()
-n3=n1.lower()
-n4=n2.lower()
-c=0
-for i in n3:
-    if i in n4:
-        c=c+1
-if c==len(n3)==len(n4):
+def anagram(s):
+    a=[]
+    for ch in s.lower():
+        a.append(ch)
+    d={}
+    for ch in a:
+        if ch not in d:
+            d[ch]=1
+        else:
+            d[ch]=d[ch]+1
+    return d
+s1=input()
+s2=input()
+a=anagram(s1)
+b=anagram(s2)
+if a==b:
     print("True")
 else:
     print("False")
