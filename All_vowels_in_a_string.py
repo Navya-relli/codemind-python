09a6d9af9450daf1b169f5dcea85ea6aa38e2bb1
Navya <97428614+Navya-relli@users.noreply.github.com>
2022-10-16 12:58:05 +0530

    s=list(map(str,input().split())) b=[] c=[] for i in s:     for j in i:         if j=='a':             b.append(j)         elif j=='e':             b.append(j)         elif j=='i':             b.append(j)         elif j=='o':             b.append(j)         elif j=='u':             b.append(j)         elif j=="A":             c.append(j)         elif j=="E":             c.append(j)         elif j=="I":             c.append(j)         elif j=="O":             c.append(j)         elif j=='U':             c.append(j) n=len(set(b)) m=len(set(c)) if n==5:     print('True') elif m==5:     print('True') else:     print("False")

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,12 +1,41 @@
-n=input()
-k='aeiou'
-p=[]
-c=0
-for i in n:
-    if i in k and i not in p:
-        p.append(i)
-        
-if len(p)==5:
-    print("True")
+s=list(map(str,input().split()))
+b=[]
+c=[]
+for i in s:
+    for j in i:
+        if j=='a':
+            b.append(j)
+        elif j=='e':
+            b.append(j)
+        elif j=='i':
+            b.append(j)
+        elif j=='o':
+            b.append(j)
+        elif j=='u':
+            b.append(j)
+        elif j=="A":
+            c.append(j)
+        elif j=="E":
+            c.append(j)
+        elif j=="I":
+            c.append(j)
+        elif j=="O":
+            c.append(j)
+        elif j=='U':
+            c.append(j)
+n=len(set(b))
+m=len(set(c))
+if n==5:
+    print('True')
+elif m==5:
+    print('True')
 else:
     print("False")
+        
+        
+        
+        
+        
+        
+        
+        
