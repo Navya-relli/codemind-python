803ac1320c7add90a2f731818549f09488566940
Navya <97428614+Navya-relli@users.noreply.github.com>
2022-10-16 10:46:52 +0530

    s1=input().lower() s2=input().lower() s1=s1.split(' ') s2=s2.split(' ') for i in s2:     for j in s1:         if i==j:             print(i,end=' ')

--- common_words.py
+++ common_words.py
@@ -1,11 +1,8 @@
-s1=input()
-s2=input()
-s1=s1.lower()
-s2=s2.lower()
-s3=s1.split()
-s4=s2.split()
-k=[]
-for i in s4:
-    if i in s3:
-        k.append(i)
-print(*k)
+s1=input().lower()
+s2=input().lower()
+s1=s1.split(' ')
+s2=s2.split(' ')
+for i in s2:
+    for j in s1:
+        if i==j:
+            print(i,end=' ')
