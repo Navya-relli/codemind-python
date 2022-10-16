fd76ef8ca7f1b48d99f7bada6e102599f42cb599
Navya <97428614+Navya-relli@users.noreply.github.com>
2022-10-16 12:41:25 +0530

    x=list(map(str,input().split())) n=[] s=0 l=['a','e','i','o','u'] for i in x:     for j in i:         if j in l:             s+=1     n.append(s)     s=0 print(max(n))

--- maximum_vowel_count.py
+++ maximum_vowel_count.py
@@ -1,14 +1,11 @@
-n=str(input())
-a=n.lower()
-b=n.split()
-d='aeiou'
-m=[]
-for i in b:
+x=list(map(str,input().split()))
+n=[]
+s=0
+l=['a','e','i','o','u']
+for i in x:
+    for j in i:
+        if j in l:
+            s+=1
+    n.append(s)
     s=0
-    c=list(i)
-    for j in c:
-        if j in d:
-            s=s+1
-    m.append(s)
-print(max(m))
-        
+print(max(n))
