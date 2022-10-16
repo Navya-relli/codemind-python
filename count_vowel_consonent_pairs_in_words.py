b5b804f2630e978cf33e31b9dd9d2d08d03a1a25
Navya <97428614+Navya-relli@users.noreply.github.com>
2022-10-16 12:07:15 +0530

    x=list(map(str,input().split())) a='AEIOUaeiou' c=0 d=0 for i in x:     for j in range(len(i)//2):         if ((i[j] in a) and (i[len(i)-j-1]not in a)) or ((i[j] not in a) and (i[len(i)-j-1] in a )):             c+=1 print(c)

---
+++ count_vowel_consonent_pairs_in_words.py
@@ -0,0 +1,9 @@
+x=list(map(str,input().split()))
+a='AEIOUaeiou'
+c=0
+d=0
+for i in x:
+    for j in range(len(i)//2):
+        if ((i[j] in a) and (i[len(i)-j-1]not in a)) or ((i[j] not in a) and (i[len(i)-j-1] in a )):
+            c+=1
+print(c)
