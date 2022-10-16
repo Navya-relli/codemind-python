5a787b0b201108151a3976771cb30d8585a2942e
Navya <97428614+Navya-relli@users.noreply.github.com>
2022-10-16 12:52:02 +0530

    s=input() v='aeiou' l=[] c=0 for i in s:     if i=='a' or i=='e' or i=='i' or i=='o' or i=='u':         l.append(i) if len(set(l))==len(set(v)):     print(0) else:     for j in v:         for k in list(set(l)):             if j==k:                 c+=1         if c==0:             print(j,end=' ')         c=0

--- vowels_not_in_a_string.py
+++ vowels_not_in_a_string.py
@@ -1,13 +1,17 @@
-n=input()
-k='aeiou'
-p=[]
+s=input()
+v='aeiou'
+l=[]
 c=0
-for i in n:
-    if i in k and i  not in p:
-        p.append(i)
-for i in k:
-    if i not in p:
-        c=c+1
-        print(i,end=" ")
-if c==0:
+for i in s:
+    if i=='a' or i=='e' or i=='i' or i=='o' or i=='u':
+        l.append(i)
+if len(set(l))==len(set(v)):
     print(0)
+else:
+    for j in v:
+        for k in list(set(l)):
+            if j==k:
+                c+=1
+        if c==0:
+            print(j,end=' ')
+        c=0
