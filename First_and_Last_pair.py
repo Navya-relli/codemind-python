f113fb7d12d3dee678e516ed0ab7b5fa8183dda3
Navya <97428614+Navya-relli@users.noreply.github.com>
2022-10-24 21:54:35 +0530

    n=int(input()) a=list(map(int,input().split())) l=[] if n%2!=0:     for i in range(n//2+1):         if i!=(n-1)-i:             print(a[i],end=' ')             print(a[(n-1)-i],end=' ')     print(a[i],end=' ')     print('0') elif n%2==0:     for i in range(n//2):         if i!=(n-1)-i:             print(a[i],end=' ')             print(a[(n-1)-i],end=' ')

--- First_and_Last_pair.py
+++ First_and_Last_pair.py
@@ -1,17 +1,16 @@
 n=int(input())
 a=list(map(int,input().split()))
-s=[]
+l=[]
 if n%2!=0:
-    for i in range(0,n//2+1):
-        if a[i]!=a[n-1-i]:
-            s.append(a[i])
-            s.append(a[n-1-i])
-        else:
-            s.append(a[i])
-            s.append(0)
-else:
-    for i in range(0,n//2):
-        s.append(a[i])
-        s.append(a[n-1-i])
-print(*s)
-                
+    for i in range(n//2+1):
+        if i!=(n-1)-i:
+            print(a[i],end=' ')
+            print(a[(n-1)-i],end=' ')
+    print(a[i],end=' ')
+    print('0')
+elif n%2==0:
+    for i in range(n//2):
+        if i!=(n-1)-i:
+            print(a[i],end=' ')
+            print(a[(n-1)-i],end=' ')
+    
