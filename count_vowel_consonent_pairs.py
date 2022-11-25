55e284d63e6f627d8c657fa0c6892a6a1de06e39
Navya <97428614+Navya-relli@users.noreply.github.com>
2022-11-25 17:48:46 +0530

    s=input() s=s.lower() c=0 v="aeiou" a,b=0,len(s)-1 while a<b:     if s[a].isalpha() and s[b].isalpha():         if (s[a] in v and s[b] not in v) or (s[a] not in v and s[b] in v):             c+=1     a+=1     b-=1 print(c)

--- count_vowel_consonent_pairs.py
+++ count_vowel_consonent_pairs.py
@@ -5,7 +5,7 @@ v="aeiou"
 a,b=0,len(s)-1
 while a<b:
     if s[a].isalpha() and s[b].isalpha():
-        if (s[a]in v and s[b] not in v) or (s[a] not in v and s[b] in v):
+        if (s[a] in v and s[b] not in v) or (s[a] not in v and s[b] in v):
             c+=1
     a+=1
     b-=1
