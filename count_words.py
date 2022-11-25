a9ae32bb4f72c680d8d8f97838562a87bea41e0f
Navya <97428614+Navya-relli@users.noreply.github.com>
2022-11-25 17:44:24 +0530

    s=list(map(str,input().split())) c=0 for i in s:     if i[0]=='a' or i[0]=='e' or i[0]=='i' or i[0]=='o' or i[0]=='u' or i[0]=='A' or i[0]=='E' or i[0]=='I' or i[0]=='O' or i[0]=='U':         if i[len(i)-1]!='a' or i[len(i)-1]!='e' or i[len(i)-1]!='i' or i[len(i)-1]!='o' or i[len(i)-1]!='u' or i[len(i)-1]!='A' or i[len(i)-1]!='E' or i[len(i)-1]!='I' or i[len(i)-1]!='O' or i[len(i)-1]!='U':             c+=1 print(c)

--- count_words.py
+++ count_words.py
@@ -1,8 +1,7 @@
-x=list(map(str,input().split()))
+s=list(map(str,input().split()))
 c=0
-l=['a','e','i','o','u','A','E','I','O','U']
-for i in x:
-    n=len(i)
-    if (i[0] in l) and (i[n-1] not in l):
-        c+=1
+for i in s:
+    if i[0]=='a' or i[0]=='e' or i[0]=='i' or i[0]=='o' or i[0]=='u' or i[0]=='A' or i[0]=='E' or i[0]=='I' or i[0]=='O' or i[0]=='U':
+        if i[len(i)-1]!='a' or i[len(i)-1]!='e' or i[len(i)-1]!='i' or i[len(i)-1]!='o' or i[len(i)-1]!='u' or i[len(i)-1]!='A' or i[len(i)-1]!='E' or i[len(i)-1]!='I' or i[len(i)-1]!='O' or i[len(i)-1]!='U':
+            c+=1
 print(c)
