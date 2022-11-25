58d86d63b6557a6589cf1a5042c38a6502563466
Navya <97428614+Navya-relli@users.noreply.github.com>
2022-11-25 17:31:05 +0530

    n=input() z=n.split() l=[] for i in z:     vowel=sum(letter in 'aeiou' for letter in i.lower())     l.append(vowel) print(l.count(max(l)))

--- count_words_with_maximum_vowels.py
+++ count_words_with_maximum_vowels.py
@@ -1,12 +1,7 @@
-x=list(map(str,input().split()))
-n=[]
-s=0
-l=['a','e','i','o','u']
-for i in x:
-    for j in i:
-        if j in l:
-            s+=1
-    n.append(s)
-    s=0
-m=max(n)
-print(n.count(m))
+n=input()
+z=n.split()
+l=[]
+for i in z:
+    vowel=sum(letter in 'aeiou' for letter in i.lower())
+    l.append(vowel)
+print(l.count(max(l)))
