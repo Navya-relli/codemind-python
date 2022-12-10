2d6b4a2d9962bd8b284b50dcf4d299d6ba8cb0f3
Navya <97428614+Navya-relli@users.noreply.github.com>
2022-12-10 09:28:01 +0530

    s=input() s=s.lower() s=s.replace(" ","") c=0 for i in s:     x=s.count(i)     if x==1:         c+=1 if c==0:     print("-1") else:     print(c)

--- count_unique_characters.py
+++ count_unique_characters.py
@@ -1,7 +1,12 @@
-z=input().lower()
-s=0
-for i in sorted(z):
-    if i>='a' and i<='z':
-        if z.count(i)==1:
-            s+=1
-print(s)
+s=input()
+s=s.lower()
+s=s.replace(" ","")
+c=0
+for i in s:
+    x=s.count(i)
+    if x==1:
+        c+=1
+if c==0:
+    print("-1")
+else:
+    print(c)
